cdb3a5932154967584b3037d8c1c1432490f3786
PejkovskiP <149232521+PejkovskiP@users.noreply.github.com>
2023-11-16 14:28:10 +0100

    block scope

---
+++ 03-global-function-scope/04-block-scope/script.js
@@ -0,0 +1,29 @@
+const  x = 100;
+
+if (true) {
+    const y = 200;
+    console.log(x + y);
+}
+
+// console.log(x + y);
+
+for (let i = 0; i <= 10; i++) {
+    console.log(i);
+}
+
+if(true) {
+    const a = 500;
+    let b = 600;
+    var c = 700;
+}
+
+console.log(c); // var is not a block scope wile const and let are
+
+function run() {
+    var d = 100;
+    console.log(d);
+}
+
+run();
+
+// console.log(d);
